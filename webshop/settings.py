b91bcacfa31aabdb8bfe220660ac74e529afa710
BernarBerdikul <DaBEERman32215@gmail.com>
2020-02-13 17:15:58 +0600

    version2

--- webshop/settings.py
+++ webshop/settings.py
@@ -67,12 +67,8 @@ WSGI_APPLICATION = 'webshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CoolMarkt',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
