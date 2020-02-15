049a9d7f6958620f2d9318963923c229be3e8cce
BernarBerdikul <DaBEERman32215@gmail.com>
2020-02-15 23:04:57 +0600

    version8 no errors

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 command = '/root/code/CoolMarkt/venv/bin/gunicorn'
 pythonpath = '/root/code/CoolMarkt/CoolMarkt'
-bind = '0.0.0.0:8001'
+bind = '178.170.221.163:8000'
 workers = 3
 user = 'root'
 limit_request_fields = 32000
